f13c7e773157b39a64bdcedf791ee4aed85b3cb9
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-05-06 20:07:00 +0530

    made easy with default parameter groups

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # terraform-aws-aurora-serverless
 
 This module is used to create serverless aws aurora cluster with either aurora-mysql or aurora-postgresql as database engine. 
-It is used to create aws glue catalog database and aws crawler runs in rds database to populate the glue catalog database.
-RDS database and glue catalog database is connected using jdbc.
 This module also creates a separate VPC with subnets passed
+Currently Postgresql 10.7 and MySQL 5.6.10a are only specific engine versions supported for serverless.s
--- parameter-groups.tf
+++ parameter-groups.tf
@@ -1,3 +1,5 @@
+## Currently only PostgreSQL 10.7 and MySQL 5.6 Engines are supported #######################
+## These are the default parameter groups (postgresql10 and aurora5.6) that may be created ##
 
 resource "aws_db_parameter_group" "aurora_db_postgres_parameter_group" {
   count       = var.db_parameter_group_name == "" && var.engine == "aurora-postgresql" ? 1 : 0
@@ -15,14 +17,14 @@ resource "aws_rds_cluster_parameter_group" "aurora_cluster_postgres_parameter_gr
 
 resource "aws_db_parameter_group" "aurora_db_mysql_parameter_group" {
   count       = var.db_parameter_group_name == "" && var.engine == "aurora-mysql" ? 1 : 0
-  name        = "aurora-db-postgres10-parameter-group"
-  family      = "aurora-postgresql10"
-  description = "aurora-db-postgres10-parameter-group"
+  name        = "aurora-db-mysql5-6-parameter-group"
+  family      = "aurora5.6"
+  description = "aurora-db-mysql5-6-parameter-group"
 }
 
 resource "aws_rds_cluster_parameter_group" "aurora_cluster_mysql_parameter_group" {
   count       = var.db_cluster_parameter_group_name == "" && var.engine == "aurora-mysql" ? 1 : 0
-  name        = "aurora-postgres10-cluster-parameter-group"
-  family      = "aurora-postgresql10"
-  description = "aurora-postgres10-cluster-parameter-group"
+  name        = "aurora-mysql5-6-cluster-parameter-group"
+  family      = "aurora5.6"
+  description = "aurora-mysql5-6-cluster-parameter-group"
 }

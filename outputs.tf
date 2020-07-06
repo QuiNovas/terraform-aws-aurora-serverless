ac1e231ede1782d615bf92602c39d30b2fa0ddf9
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2020-07-06 14:39:23 +0530

    added cluster parameter group name as output

--- outputs.tf
+++ outputs.tf
@@ -43,9 +43,14 @@ output "database_subnets" {
   value       = module.vpc.database_subnets
 }
 
-output "db_parameter_group_name" {
+output "db_cluster_parameter_group_name" {
   description = "name of db parameter group"
   value       = local.db_parameter_group_name
+}
+
+output "db_parameter_group_name" {
+  description = "name of db cluster parameter group"
+  value       = local.db_cluster_parameter_group_name
 }
 
 output "hosted_zone_id" {
